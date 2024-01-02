83a2d28d5ec97bfb4402f76f4c47198d82678226
murtazarangwala99 <murtazarangwala95@gmail.com>
2024-01-02 14:48:45 +0530

    Remove PostCss in Production.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        myblack: "#0D0D0D",
+        skyblue: "#00BAF2",
+        myblue: "#2bc9f8",
+        blue: "#0F4A8A",
+        blue2: "#225ea0",
+        mywhite: "#F5F7FA",
+      },
+    },
+    theme: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+    },
   },
   plugins: [],
 };
